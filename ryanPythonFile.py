d1926e8b309f01fde1450d17b6ce93bb49e19250
rgendel <ryangendel@u.northwestern.edu>
2020-01-09 13:07:33 -0700

    more stuff

--- ryanPythonFile.py
+++ ryanPythonFile.py
@@ -1,5 +1,5 @@
 ryan = "awesome, i mean maybe"
-
+MOREGS ="HELLLO"
 a = "why we have no water, "
 b = "im thirstyyyyyy, "
 c = "send helpppp!!! SOS"
@@ -10,4 +10,13 @@ g = "g lion"
 
 p = "p lion"
 
+GUNIT="GGGGGGGUNIT"
+
 e = "e  lion"
+
+#more stufffff
+#bout to mess this github up 
+
+#master of puppets
+
+metallica = "sleep now in the fire"
